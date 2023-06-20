7cd51452ecbd6e8af0f4dab27c248197d7b374bc
Wing Seng <chewwingseng@gmail.com>
2023-06-20 11:41:55 +0800

    update package dependency

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ISSEvents",
-            dependencies: []),
+            dependencies: ["ISSTheme",]),
         .testTarget(
             name: "ISSEventsTests",
             dependencies: ["ISSEvents"]),
